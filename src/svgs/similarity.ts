785a1ff3a63e2513347f3d25d596d96c8df0bba8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-23 19:06:26 +0100

    dev: added xxx

--- src/svgs/similarity.ts
+++ src/svgs/similarity.ts
@@ -5,7 +5,7 @@ export const similarity = (t: SVGTool) => {
   const g = querySelector("svg.similarity g", SVGGElement);
   for (let x = 0; x < 5; x++) {
     for (let y = 0; y < 4; y++) {
-      const myClass = x > 1 && y < 2 ? "" : "full";
+      const myClass = x > 1 && y < 2 ? "xxx" : "full";
       t.createCircle(g, x * 20 + 100, y * 20 + 20, 5, {
         class: myClass,
         delay: 0,
